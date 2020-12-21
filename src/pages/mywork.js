db8f62568c7268863f3b314959a8ee04dc6a3d87
Beata Tomczak <tma.btty@gmail.com>
2020-12-21 15:31:01 +0100

    Basic setup

---
+++ src/pages/mywork.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const MyWork = () => (
+  <Layout>
+    <SEO title="Page two" />
+    <p>My Work</p>
+  </Layout>
+)
+
+export default MyWork
